284e77a879c8e4b1446ea3731f79dcbe240d2f26
Victor Thompson <thompsonvictor1206@gmail.com>
2024-11-03 14:53:48 +0100

    fix footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ import { FaInstagram } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <div className="flex flex-col bg-light justify-between h-full lg:h-[500px] overflow-hidden lg:flex-row lg:px-0">
+    <div className="flex flex-col bg-light justify-between h-full lg:h-[500px] overflow-hidden lg:flex-row">
       <div className="w-full lg:w-1/2  py-10 lg:py-0">
         <img
           src={contact_footer}
@@ -18,16 +18,16 @@ const Footer = () => {
           <p className="text-gray-800 text-xl pb-10 font-garamond font-medium ">
             CONTACT US
           </p>
-          <div className="mb-10">
+          <div className="mb-5">
             <div className="flex gap-10 mb-5">
-              <p className="w-20 text-gray-800 font-medium">Email:</p>
+              <p className="w-16 text-gray-800 font-medium">Email:</p>
               <div className="text-gray-700">
                 <p className="pb-1">johnottoh@shortletelders.com</p>
                 <p>mullyshaven@shortletelders.com</p>
               </div>
             </div>
             <div className="flex gap-10">
-              <p className="w-20 text-gray-800 font-medium">Number:</p>
+              <p className="w-16 text-gray-800 font-medium">Number:</p>
               <div className="text-gray-700">
                 <p className="pb-1">+2347034578122 ( Calls & Whatsapp)</p>
                 <p>+2347067826403 (Whatsapp Only)</p>
