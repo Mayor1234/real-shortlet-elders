fa55ed8b4102713d9a13c3dda68ea749b901f6cd
Victor Thompson <thompsonvictor1206@gmail.com>
2024-11-19 22:18:52 +0100

    added google site verification

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(),
-//     VitePWA({
-//       registerType: 'autoUpdate',
-//       manifest: {
-//         name: 'My PWA App',
-//         short_name: 'PWAApp',
-//         description: 'My awesome Progressive Web App!',
-//         theme_color: '#ffffff',
-//         background_color: '#ffffff',
-//         display: 'standalone',
-//         start_url: '/',
-//         icons: [
-//           {
-//             src: '/icon-192x192.png',
-//             sizes: '192x192',
-//             type: 'image/png',
-//           },
-//           {
-//             src: '/icon-512x512.png',
-//             sizes: '512x512',
-//             type: 'image/png',
-//           },
-//         ],
-//       },
-//   ],
-// })
-
 // vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
@@ -80,4 +48,5 @@ export default defineConfig({
       },
     }),
   ],
+  base: '/',
 });
