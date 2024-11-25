e472f0e04d90a9db368c208ac133ddd4de6c074a
Victor Thompson <thompsonvictor1206@gmail.com>
2024-11-25 17:06:47 +0100

    added description meta tag in the header

--- src/components/hero-banner/HeroBanner.tsx
+++ src/components/hero-banner/HeroBanner.tsx
@@ -9,11 +9,11 @@ const HeroBanner = () => {
       <div className="w-full container mx-auto">
         <div className="absolute -z-10 px-5 font-normal h-[calc(85vh+80px)] lg:h-[calc(100vh+80px)] flex justify-center gap-5 flex-col items-start lg:gap-8 lg:px-0">
           <AnimatedSection direction={{ initial_x: 0, initial_y: 50 }}>
-            <h2 className="max-w-md font-merriweather text-3xl text-center font-medium uppercase leading-[45px] lg:leading-[60px] text-[#f2f2f2] lg:max-w-[750px] lg:text-5xl lg:text-start pb-10">
+            <h1 className="max-w-md font-merriweather text-3xl text-center font-medium uppercase leading-[45px] lg:leading-[60px] text-[#f2f2f2] lg:max-w-[750px] lg:text-5xl lg:text-start pb-10">
               Make Your Home <br />
               Rain Investments This December
-            </h2>
-            <p className="max-w-md font-garamond leading-[28px] text-center mb-5 text-[20px] lg:text-[28px] tracking-wide text-[#f1f5f9] lg:text-start lg:max-w-[750px] lg:leading-[35px]">
+            </h1>
+            <p className="max-w-md font-garamond px-5 leading-[28px] text-center mb-5 text-[20px] lg:text-[28px] tracking-wide text-[#f1f5f9] lg:text-start lg:max-w-[750px] lg:leading-[35px] lg:px-0">
               With a one time home lease payment & 10% - 15% ROI on your capital
               investments … This december is bound to be a financial bliss..
             </p>
